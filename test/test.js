904d628b8f0b6f5c305596bb60f11724451a47ee
Elisabeth <elisabeth.wadler@edu.fh-joanneum.at>
2024-01-01 21:30:44 +0100

    Add Unit Tests

---
+++ test/test.js
@@ -0,0 +1,80 @@
+const express = require('express');
+const pool = require('../db');
+const app = express();
+const assert = require('assert');
+const supertest = require('supertest');
+
+
+describe('GET /', () => {
+  it('should return a list of cats', (done) => {
+    supertest(app)
+      .get('/')
+      .expect(200)
+      .end((err, res) => {
+        if (err) return done(err);
+        const cats = res.body.cats;
+        assert(Array.isArray(cats), 'Response should contain an array of cats');
+        assert(cats.length > 0, 'Array of cats should not be empty');
+        done();
+      });
+  });
+});
+
+describe('POST /', () => {
+  it('should add a new cat', (done) => {
+    const newCat = {
+      name: 'Whiskers',
+      breed: 'Tabby',
+    };
+
+    supertest(app)
+      .post('/')
+      .send(newCat)
+      .expect(200)
+      .end((err, res) => {
+        if (err) return done(err);
+        const message = res.body.message;
+        assert.strictEqual(message, 'Succesfully added cat', 'Unexpected message');
+        done();
+      });
+  });
+});
+
+describe('GET /setup', () => {
+  it('should create a table if it does not exist', (done) => {
+    supertest(app)
+      .get('/setup')
+      .expect(200)
+      .end((err, res) => {
+        if (err) return done(err);
+        const message = res.body.message;
+        assert.strictEqual(message, 'Succesfully created table', 'Unexpected message');
+        done();
+      });
+  });
+
+  it('should handle table already exists scenario', (done) => {
+    supertest(app)
+      .get('/setup')
+      .expect(200)
+      .end((err, res) => {
+        if (err) return done(err);
+        const message = res.body.message;
+        assert.strictEqual(message, 'Table already exists', 'Unexpected message');
+        done();
+      });
+  });
+});
+
+describe('GET /ping', () => {
+  it('should respond with "pong!"', (done) => {
+    supertest(app)
+      .get('/ping')
+      .expect(200)
+      .expect('Content-Type', 'text/plain')
+      .expect((res) => {
+        assert.strictEqual(res.text, 'pong!', 'Unexpected response text');
+      })
+      .end(done);
+  });
+});
